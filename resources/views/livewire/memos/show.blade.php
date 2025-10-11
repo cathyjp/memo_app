de2cf560a01ee1c0b18f51e9e1c9a87a3121b3b8
cathyjp <p0_10q01k@yahoo.co.jp>
2025-10-11 18:02:49 +0900

    編集機能作成

--- resources/views/livewire/memos/show.blade.php
+++ resources/views/livewire/memos/show.blade.php
@@ -5,10 +5,15 @@ use App\Models\Memo;
 
 state(['memo' => fn(Memo $memo) => $memo]);
 
+$edit = function () {
+    return redirect()->route('memos.edit', $this->memo);
+};
+
 ?>
 
 <div>
     <a href="{{ route('memos.index') }}">戻る</a>
     <h1>{{ $memo->title }}</h1>
     <p>{!! nl2br(e($memo->body)) !!}</p>
+    <button wire:click="edit">編集する</button>
 </div>
