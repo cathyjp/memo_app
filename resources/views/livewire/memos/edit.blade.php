f80189384a52c9188401b49b2248ace2f8e950bd
cathyjp <p0_10q01k@yahoo.co.jp>
2025-10-12 11:37:34 +0900

    優先度項目を追加

--- resources/views/livewire/memos/edit.blade.php
+++ resources/views/livewire/memos/edit.blade.php
@@ -4,12 +4,13 @@ use function Livewire\Volt\{state, mount, rules};
 use App\Models\Memo;
 
 // フォームの状態を管理
-state(['memo', 'title', 'body']);
+state(['memo', 'title', 'body', 'priority']);
 
 // バリデーション
 rules([
     'title' => 'required|string|max:50',
     'body' => 'required|string|max:2000',
+    'priority' => 'required|integer|min:1|max:3',
 ]);
 
 // ルートモデルバインディングはmountでまとめて行う
@@ -17,6 +18,7 @@ mount(function (Memo $memo) {
     $this->memo = $memo;
     $this->title = $memo->title;
     $this->body = $memo->body;
+    $this->priority = $memo->priority;
 });
 
 $update = function () {
@@ -47,6 +49,18 @@ $update = function () {
             @enderror
             <br>
             <textarea wire:model="body" id="body"></textarea>
+        </p>
+        <p>
+            <label for="priority">優先度</label>
+            @error('priority')
+                <span class="error"> ( {{ $message }} ) </span>
+            @enderror
+            <br>
+            <select wire:model="priority" id="priority">
+                <option value="1">低</option>
+                <option value="2">中</option>
+                <option value="3">高</option>
+            </select>
         </p>
         <button type="submit">更新</button>
     </form>
