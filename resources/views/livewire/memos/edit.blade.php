092c0b2986e244852a45ed973614bb5c4c54527f
cathyjp <p0_10q01k@yahoo.co.jp>
2025-10-12 09:33:30 +0900

    バリデーション実装

--- resources/views/livewire/memos/edit.blade.php
+++ resources/views/livewire/memos/edit.blade.php
@@ -1,10 +1,16 @@
 <?php
 
-use function Livewire\Volt\{state, mount};
+use function Livewire\Volt\{state, mount, rules};
 use App\Models\Memo;
 
 // フォームの状態を管理
 state(['memo', 'title', 'body']);
+
+// バリデーション
+rules([
+    'title' => 'required|string|max:50',
+    'body' => 'required|string|max:2000',
+]);
 
 // ルートモデルバインディングはmountでまとめて行う
 mount(function (Memo $memo) {
@@ -14,6 +20,7 @@ mount(function (Memo $memo) {
 });
 
 $update = function () {
+    $this->validate();
     $this->memo->update($this->all());
     return redirect()->route('memos.show', $this->memo);
 };
@@ -26,11 +33,19 @@ $update = function () {
 
     <form wire:submit="update">
         <p>
-            <label for="title">タイトル</label><br>
+            <label for="title">タイトル</label>
+            @error('title')
+                <span class="error"> ( {{ $message }} ) </span>
+            @enderror
+            <br>
             <input type="text" wire:model="title" id="title">
         </p>
         <p>
-            <label for="body">本文</label><br>
+            <label for="body">本文</label>
+            @error('body')
+                <span class="error"> ( {{ $message }} ) </span>
+            @enderror
+            <br>
             <textarea wire:model="body" id="body"></textarea>
         </p>
         <button type="submit">更新</button>
